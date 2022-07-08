ec58ec2231cf04d1b6b3b6746621719102569198
Andrey <finkons87@gmail.com>
2022-07-08 16:23:10 +0300

    change button scale

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -14,20 +14,16 @@ export const Button = styled.button`
   background-color: #e4e9ed;
   border: 1px solid #030e1845;
   border-radius: 10px;
-  transform: scale(1);
   transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1),
     transform 250ms cubic-bezier(0.4, 0, 0.2, 1),
     color 250ms cubic-bezier(0.4, 0, 0.2, 1);
   &:nth-of-type(1):hover{
-    transform: scale(1.1);
     background-color: rgb(173, 241, 167);
   }
   &:nth-of-type(2):hover{
-    transform: scale(1.1);
     background-color: rgb(206, 206, 206);
   }
   &:nth-of-type(3):hover{
-    transform: scale(1.1);
     background-color: rgb(241, 167, 167);
   }
 `;
